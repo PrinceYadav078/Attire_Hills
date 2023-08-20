46ff3906cb8840ce71c9d43d5fcc26e075dda702
princeyadav283203@gmail.com <princeyadav283203@gmail.com>
2023-08-20 15:20:06 +0530

    product card completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/components/Pages/HomePage/HomePage';
-import Product from './customer/components/Product/ProductCard';
+import Product from './customer/components/Product/Product';
+
 
 function App() {
   return (
